8fe42ddf0536185e2255eb7d9334abb1a507337b
Brandon Kudo and Michael Arrington <michael@>
2017-09-25 16:22:53 -0700

    idk

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,11 @@
-
-
-
-
-
-
 $(document).ready(function() {
   $("form#newTasks").submit(function(event) {
     event.preventDefault();
-    var newTasksVal = $("#task").val();
-    var output = '<li>' + newTasksVal + '</li>'
-
-
-    $("#output").append(output);
+    var output = '<li>' + $("#task").val() + '</li>'
+   $("input#task").val("");
+   $("#output").append(output);
     $("#output").children("li").click(function() {
       $(this).remove();
     });
-
   });
 });
